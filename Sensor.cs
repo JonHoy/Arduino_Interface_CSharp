b87c4e38b805d9b241067da115c201deea8c7269
Jonathan Hoy <jonhoy34@gmail.com>
2014-10-05 16:45:42 -0700

    Added Sensor class

---
+++ Sensor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ArduinoClass
+{
+    class Sensor
+    {
+        private Arduino myArduino; // the arduino that is hooked up to the sensor
+        private int analogPin; // the analog pin number that the sensor is using
+        private double bitCount = 10; // A/D resolution of the arduino
+        private double minVoltage = 0;
+        private double maxVoltage = 5;
+
+
+        public Sensor(ref Arduino Board, int Pin)
+        {
+            this.myArduino = Board;
+            this.analogPin = Pin;
+        }
+        public double getSensorReading()
+        {
+            var val = (double) myArduino.AnalogRead(analogPin);
+            var voltage = val / (Math.Pow(2, bitCount) - 1.0) * (maxVoltage - minVoltage) + minVoltage;
+            return voltage;
+        }
+ 
+
+    }
+}
